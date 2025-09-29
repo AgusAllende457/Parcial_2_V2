0f8a76dd671081e5f950ad56075cbb388c65680b
Carolina <carofacello.13@gmail.com>
2025-09-29 16:36:33 -0300

    creacion de la entidad automovil

---
+++ HybridDDDArchitecture/Domain/Validators/AutomovilValidator.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Entities;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Validators
+{
+    public class AutomovilValidator : AbstractValidator<Automovil>
+    {
+        public AutomovilValidator()
+        {
+            RuleFor(a => a.Marca).NotEmpty();
+            RuleFor(a => a.Modelo).NotEmpty();
+            RuleFor(a => a.Color).NotEmpty();
+        }
+
+    }
+}
---
+++ HybridDDDArchitecture/Infrastructure/Repositories/Sql/AutomovilRepository.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Infraestructure.Repositories.Sql;
+using Application.Repositories;
+
+namespace Infrastructure.Repositories.Sql
+{
+    internal class AutomovilRepository : BaseRepository<Automovil>, IAutomovilRepository
+    {
+
+        public AutomovilRepository(StoreDbContext context) : base(context) { }
+    }
+
+}
